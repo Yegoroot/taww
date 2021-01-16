a938ccd014d878ea97fe2628156c576e459023af
Vasya094 <73393463+Vasya094@users.noreply.github.com>
2021-01-16 12:04:50 +0300

    Adding Main Tabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 
-function App() {
+const useStyles = makeStyles( {
+
+  root: {
+    flexGrow: 1,
+    display: "flex",
+    height: "100%",
+    flexDirection: "column",
+    justifyContent: "flex-end"
+  },
+});
+
+export default function IconLabelTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+    <Paper square >
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="fullWidth"
+        indicatorColor="secondary"
+        textColor="secondary"
+        aria-label="icon label tabs example"
+      >
+        <Tab icon={<HomeOutlinedIcon />} label="Home" />
+        <Tab icon={<FavoriteIcon />} label="FAVORITES" />
+        <Tab icon={<PersonPinIcon />} label="NEARBY" />
+        <Tab icon={<PersonPinIcon />} label="NEARBY" />
+        <Tab icon={<PersonOutlineOutlinedIcon />} label="My Profile" />
+      </Tabs>
+      </Paper>
     </div>
   );
 }
 
-export default App;
